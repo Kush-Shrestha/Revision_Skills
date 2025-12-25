984e9121e6b8f5b6d53a500a04f781ed482c6331
Kushal Shrestha <shresthakushal097@gmail.com>
2025-12-25 21:21:34 +0545

    Function declaration vs expression

--- web/js/Javascript_Learning/day3.js
+++ web/js/Javascript_Learning/day3.js
@@ -7,12 +7,41 @@ function ask(question, yes, no) {
 }
 
 function showOk() {
-  alert( "You agreed." );
+  alert("You agreed.");
 }
 
 function showCancel() {
-  alert( "You canceled the execution." );
+  alert("You canceled the execution.");
 }
 
 // usage: functions showOk, showCancel are passed as arguments to ask
-ask("Do you agree?", showOk, showCancel);
+ask("Do you agree?", showOk, showCancel);// these are the options that you get 
+
+//Function Declaration Vs Function Expression
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+
+// Function Expression
+let sum = function (a, b) {
+  return a + b;
+};
+
+
+//A Function Declaration can be called earlier than it is defined.
+
+sayHi("Kush"); // Hello, Kush
+
+function sayHi(name) {
+  alert(`Hello, ${name}`);
+}
+
+// But in case of function expression there will be error 
+
+sayHi("Kush"); // error!
+
+let sayHi = function (name) {
+  alert(`Hello, ${name}`);
+};
